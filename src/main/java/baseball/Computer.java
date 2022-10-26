955390e52e8e0eee7547f5304a5764ea437dc4cf
Woojin Lee <dhkep03@gmail.com>
2022-10-26 12:48:54 +0900

    feat: 타겟 숫자 랜덤 생성

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,9 +1,18 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
-    int targetNumber;
+    int[] targetNumber = new int[3];
 
     void startGame() {
+        System.out.println("before: " + targetNumber[0]+targetNumber[1]+targetNumber[2]);
+        this.resetTargetNumber();
+        System.out.println("after: " + targetNumber[0]+targetNumber[1]+targetNumber[2]);
+    }
 
+    void resetTargetNumber() {
+        for (int i = 0; i < targetNumber.length; i++) {
+            targetNumber[i] = Randoms.pickNumberInRange(1, 9);
+        }
     }
 }
