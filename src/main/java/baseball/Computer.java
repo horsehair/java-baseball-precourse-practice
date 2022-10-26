9f7dd7415b60ab68fff67040bb1478a670279be4
Woojin Lee <dhkep03@gmail.com>
2022-10-26 13:02:37 +0900

    feat: 사용자 입력 받아 배열로 변환

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,5 +1,8 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
 
 public class Computer {
     int[] targetNumber = new int[3];
@@ -8,11 +11,27 @@ public class Computer {
         System.out.println("before: " + targetNumber[0]+targetNumber[1]+targetNumber[2]);
         this.resetTargetNumber();
         System.out.println("after: " + targetNumber[0]+targetNumber[1]+targetNumber[2]);
+        System.out.println(Arrays.toString(getInput()));
     }
 
     void resetTargetNumber() {
         for (int i = 0; i < targetNumber.length; i++) {
             targetNumber[i] = Randoms.pickNumberInRange(1, 9);
         }
+    }
+
+    int[] getInput() {
+        String input = Console.readLine();
+        int[] inputIntArray = convertStringToIntArray(input);
+        return inputIntArray;
+    }
+
+    int[] convertStringToIntArray(String numberString) {
+        char[] charArray = numberString.toCharArray();
+        int[] intArray = new int[3];
+        for (int i = 0; i < charArray.length; i++) {
+            intArray[i] = Character.getNumericValue(charArray[i]);
+        }
+        return intArray;
     }
 }
