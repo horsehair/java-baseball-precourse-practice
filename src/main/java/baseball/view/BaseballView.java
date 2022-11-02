1cac88a95bf08a49d4b86da72e673cccde92a1b7
Woojin Lee <dhkep03@gmail.com>
2022-11-02 20:52:40 +0900

    refactor: 뷰에 컨트롤러 의존성 주입

--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -1,5 +1,14 @@
 package baseball.view;
 
-public class BaseballView {
+import baseball.controller.BaseballController;
+import camp.nextstep.edu.missionutils.Console;
 
+public class BaseballView {
+	private BaseballController baseballController = new BaseballController();
+
+	public void getStringOfNumberFromClient() {
+		System.out.print("숫자를 입력해주세요 : ");
+		String stringOfNumberFromClient = Console.readLine();
+		this.baseballController.continueGame(stringOfNumberFromClient);
+	}
 }
