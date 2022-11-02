d5b32c0a7a07d0d6bf8645a6852457f88c513859
Woojin Lee <dhkep03@gmail.com>
2022-11-02 20:13:04 +0900

    feat: 컨트롤러에 서비스 의존성 주입

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,5 +1,11 @@
 package baseball.controller;
 
-public class BaseballController {
+import baseball.service.BaseballService;
 
+public class BaseballController {
+	private BaseballService baseballService = new BaseballService();
+
+	public void startGame() {
+		this.baseballService.startGame();
+	}
 }
