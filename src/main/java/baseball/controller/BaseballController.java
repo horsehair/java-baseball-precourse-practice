1a4e1040956b5c4127f397bdbc85e3ff098e2ce4
Woojin Lee <dhkep03@gmail.com>
2022-11-02 20:31:45 +0900

    feat: 서비스에 뷰 의존성 주입

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,11 +1,14 @@
 package baseball.controller;
 
 import baseball.service.BaseballService;
+import baseball.view.BaseballView;
 
 public class BaseballController {
 	private BaseballService baseballService = new BaseballService();
+	private BaseballView baseballView = new BaseballView();
 
 	void startGame() {
 		this.baseballService.startGame();
+		this.baseballView.startGame();
 	}
 }
